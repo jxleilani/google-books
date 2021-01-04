d5683c51687687f607a61052841dfcc3c102f2c6
jleilani <jxleilani@gmail.com>
2021-01-04 12:07:40 -0500

    add styling to search results

--- client/src/components/search/SearchItem.js
+++ client/src/components/search/SearchItem.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import './styles.css'
+import React from "react";
+import "./styles.css";
 
-function SearchItem({result}) {
+function SearchItem({ result }) {
   return (
     <div className="searchItem">
       <p>{result.volumeInfo.title}</p>
+      <p>{result.volumeInfo.subtitle}</p>
+      <p>Written by: {result.volumeInfo.authors}</p>
+      <div className="details">
+        <img src={result.volumeInfo.imageLinks.smallThumbnail} alt="book cover" />
+        <p>{result.volumeInfo.description}</p>
+      </div>
+
+      <div className="buttons">
+        <button>View</button>
+        <button>Save</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchItem
+export default SearchItem;
