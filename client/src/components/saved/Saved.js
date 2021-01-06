956ebd2d2ca2ae50aeaebc95af7a81dd2017d2e3
jleilani <jxleilani@gmail.com>
2021-01-06 11:55:34 -0500

    added styling to Saved page, need to connect API

--- client/src/components/saved/Saved.js
+++ client/src/components/saved/Saved.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import './styles.css'
-import SavedItem from './SavedItem'
+import React from "react";
+import "./styles.css";
+import SavedItem from "./SavedItem";
 
 function Saved() {
+  const savedBooks = [
+    {
+      title: "Harry Potter",
+      subtitle: "Subtitle of the book",
+      author: "JK Rowling",
+      description: "The book description goes here.",
+      image:
+        "http://books.google.com/books/content?id=iO5pApw2JycC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    },
+    {
+      title: "Harry Potter 2",
+      subtitle: "2 Subtitle of the book",
+      author: "2 JK Rowling",
+      description: "2 The book description goes here.",
+      image:
+        "http://books.google.com/books/content?id=iO5pApw2JycC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    },
+  ];
+
   return (
     <section>
       <div className="saved">
         <h3>Saved Books</h3>
-        <SavedItem />
+        {savedBooks.map((book) => (
+          <SavedItem key={book.title} item={book} />
+        ))}
       </div>
     </section>
-  )
+  );
 }
 
-export default Saved
+export default Saved;
--- client/src/components/saved/SavedItem.js
+++ client/src/components/saved/SavedItem.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function SavedItem() {
+function SavedItem({ item }) {
   return (
-    <div className="searchItem">
-      <p>results here....</p>
+    <div className="savedItem">
+      <p value="title">{item.title}</p>
+      <p value="subtitle">{item.subtitle}</p>
+      <p value="author">Written by: {item.author}</p>
+      <div className="details">
+        <img
+          src={item.image}
+          alt="book cover"
+        />
+        <p>{item.description}</p>
+      </div>
+      <div className="buttons">
+        <button>View</button>
+        <button>Delete</button>
+      </div>
     </div>
   )
 }
