119e9b781929172e26579db94a07f2038c14ac09
jleilani <jxleilani@gmail.com>
2021-01-10 19:57:58 -0500

    remove ghpages

---
+++ client/build/precache-manifest.3d4f65cee5b11e54171177587b3cd776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f7c649ec19fb5efc90d7e1af5e92b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209fd4dd02586f0425dc",
+    "url": "/static/css/main.0ca8c245.chunk.css"
+  },
+  {
+    "revision": "4374cebe6275eb469585",
+    "url": "/static/js/2.55ea591c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.55ea591c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209fd4dd02586f0425dc",
+    "url": "/static/js/main.15e9ca3c.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
