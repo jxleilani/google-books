fad3ebefb667d5850ec32f427ffdf234c7c003a2
jleilani <jxleilani@gmail.com>
2021-01-10 20:04:16 -0500

    deploying

--- server.js
+++ server.js
@@ -24,7 +24,12 @@ app.get("*", (req, res) => {
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-{ useNewUrlParser: true, useUnifiedTopology: true });
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
